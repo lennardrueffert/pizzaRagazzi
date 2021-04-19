d5a8b805393952c6f3d922bf981ad867626b35aa
Lennard Rüffert <lennard.rueffert@stud-mail.uni-wuerzburg.de>
2021-04-19 13:55:44 +0200

    Profile partly ported

--- assets/javascripts/functions_profile-page.js
+++ assets/javascripts/functions_profile-page.js
@@ -118,6 +118,7 @@ function setupButtonOnclicksAndInputs() {
 
 // Sends a request to update the username in the session
 function updateUsernameInDatabaseAndSession(newUsername) {
+    /*Original----------
     fetch("/profile/updateUsername", {
             method: 'POST',
             body: JSON.stringify({username: newUsername}),
@@ -131,234 +132,260 @@ function updateUsernameInDatabaseAndSession(newUsername) {
             console.log(msg);
         }
     );
+     */
 }
 
 async function uploadProfilePictureIntoDB(image) {
-    fetch('/profile/uploadProfilePicture',
-        {
-            method: 'POST',
-            body: JSON.stringify({img: image}),
-            headers: {'Content-Type': 'application/json'},
-            credentials: 'include'
-        }
-    ).then(
-        result => result.text()
-    )
+    /*Original----------
+   fetch('/profile/uploadProfilePicture',
+       {
+           method: 'POST',
+           body: JSON.stringify({img: image}),
+           headers: {'Content-Type': 'application/json'},
+           credentials: 'include'
+       }
+   ).then(
+       result => result.text()
+   )
+     */
 }
 
 // Reads username from Database and updates html
 function getUsernameFromDatabase() {
-    $.get("/getUsername", function (data, status) {
-        document.getElementById("username").textContent = data
-    }).fail(function (data, status) {
-        document.getElementById("username").textContent = "Default Name";
-        alert("Couldn't retrieve username from database");
-    });
+   document.getElementById("username").textContent = "Max"
+   /*Original----------
+  $.get("/getUsername", function (data, status) {
+      document.getElementById("username").textContent = data
+  }).fail(function (data, status) {
+      document.getElementById("username").textContent = "Default Name";
+      alert("Couldn't retrieve username from database");
+  });
+    */
 }
 
 //TODO hier evtl "getMail" route nutzen
 function getMailFromDatabase() {
-    $.get("/profile/getMail", function (data, status) {
-        document.getElementById("mail").textContent = "Email: " + data
-    }).fail(function (data, status) {
-        document.getElementById("mail").textContent = "Default Mail";
-        alert("Couldn't retrieve mail from database");
-    });
+   //Ported------------
+   document.getElementById("mail").textContent = "Email: " + "muster@mail.com"
+   /*Original----------
+   $.get("/profile/getMail", function (data, status) {
+       document.getElementById("mail").textContent = "Email: " + data
+   }).fail(function (data, status) {
+       document.getElementById("mail").textContent = "Default Mail";
+       alert("Couldn't retrieve mail from database");
+   });
+    */
 }
 
 function getGesamtpunkteFromDatabase() {
+    //Ported------------
+    document.getElementById("gesamtpunkte").textContent = "Gesamtpunkte: " + "4200"
+    /*Original----------
     $.get("/profile/getTotalPoints", function (data, status) {
         document.getElementById("gesamtpunkte").textContent = "Gesamtpunkte: " + data
     }).fail(function (data, status) {
         document.getElementById("gesamtpunkte").textContent = "Default Gesamtpunkte";
         alert("Couldn't retrieve gesamtpunkte from database");
     });
+     */
 }
 
 function getHighscoreFromDatabase() {
+    //Ported------------
+    document.getElementById("highscore").textContent = "Highscore: " + "800"
+    /*Original----------
     $.get("/profile/getHighScore", function (data, status) {
         document.getElementById("highscore").textContent = "Highscore: " + data
     }).fail(function (data, status) {
         document.getElementById("highscore").textContent = "Default Highscore";
         alert("Couldn't retrieve highscore from database");
     });
+     */
 }
 
 function getTierFromDatabase() {
+    //Ported------------
+    document.getElementById("tier").textContent = "Rang: " + "Tester";
+    /*Original----------
     $.get("/profile/getTierName", function (data, status) {
         document.getElementById("tier").textContent = "Rang: " + data
     }).fail(function (data, status) {
         document.getElementById("highscore").textContent = "Default Tier";
         alert("Couldn't retrieve tier from database");
     });
+     */
 }
 
 function getProfilePicFromDatabase() {
-    $.get("/profile/getProfilePic", function (data, status) {
-        if (data != null) {
-            document.getElementById("profile-picture").setAttribute("src", data)
-        } else document.getElementById("profile-picture").setAttribute("src", "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png");
-    }).fail(function (data, status) {
-        alert("Couldn't retrieve Profile Picture from database");
-    });
+    document.getElementById("profile-picture").setAttribute("src", "assets/images/tester_profilepicture.jpg")
+/*Original----------
+$.get("/profile/getProfilePic", function (data, status) {
+    if (data != null) {
+        document.getElementById("profile-picture").setAttribute("src", data)
+    } else document.getElementById("profile-picture").setAttribute("src", "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png");
+}).fail(function (data, status) {
+    alert("Couldn't retrieve Profile Picture from database");
+});
+ */
+
 }
 
 //onclick-function von friend aus friendlist (functions_friendlist.js)
 function setupInformationFromFriend(elm) {
-    if (document.getElementById("editProfileButton").innerText === "Speichern") {  //gegen seltenen Bug: während dem Editieren des Profiles das Profil eines Freundes anschauen
-        alert("Bitte erst Profil speichern!")
-    } else if (!viewOnly) {        //Funktion wird nur ausgeführt, wenn man auf dem eigenen Profil ist
-        const name = elm.childNodes[1].innerHTML;  //childnodes[1] gibt das "name" child von friend
+if (document.getElementById("editProfileButton").innerText === "Speichern") {  //gegen seltenen Bug: während dem Editieren des Profiles das Profil eines Freundes anschauen
+    alert("Bitte erst Profil speichern!")
+} else if (!viewOnly) {        //Funktion wird nur ausgeführt, wenn man auf dem eigenen Profil ist
+    const name = elm.childNodes[1].innerHTML;  //childnodes[1] gibt das "name" child von friend
 
-        friendGetUsernameFromDatabase(name);
-        friendGetGesamtpunkteFromDatabase(name);
-        friendGetHighscoreFromDatabase(name);
-        friendGetMailFromDatabase(name);
-        friendGetTierFromDatabase(name);
-        friendGetProfilePicFromDatabase(name);
+    friendGetUsernameFromDatabase(name);
+    friendGetGesamtpunkteFromDatabase(name);
+    friendGetHighscoreFromDatabase(name);
+    friendGetMailFromDatabase(name);
+    friendGetTierFromDatabase(name);
+    friendGetProfilePicFromDatabase(name);
 
 
-        deleteOldFriendList();          //Liste wird gelöscht, damit nur neue angezeigt wird
-        friendGetFriendsData(name);
+    deleteOldFriendList();          //Liste wird gelöscht, damit nur neue angezeigt wird
+    friendGetFriendsData(name);
 
-        hideSensibleStuff();
+    hideSensibleStuff();
 
-        document.getElementById("backToMyProfileButton").style.display = "block";  //Zurück Button visible
-        document.getElementById("chat_heading").textContent = "Chat"; //Chat-Überschrift neutral machen
+    document.getElementById("backToMyProfileButton").style.display = "block";  //Zurück Button visible
+    document.getElementById("chat_heading").textContent = "Chat"; //Chat-Überschrift neutral machen
 
-        viewOnly = true;                //in functions_friendlist.js wird der Freundeslisten hoverEffect und onclick nicht mehr ausgeführt (diese Funktion auch nicht)
-    }
+    viewOnly = true;                //in functions_friendlist.js wird der Freundeslisten hoverEffect und onclick nicht mehr ausgeführt (diese Funktion auch nicht)
+}
 }
 
 function backToMyProfile() {
-    deleteOldFriendList();          //FreundesListe wird gelöscht, damit nur die vom logged in user angezeigt werden
+deleteOldFriendList();          //FreundesListe wird gelöscht, damit nur die vom logged in user angezeigt werden
 
-    setup();
+setup();
 
-    showSensibleStuff();
+showSensibleStuff();
 
-    viewOnly = false;
+viewOnly = false;
 }
 
 function hideSensibleStuff() {
-    document.getElementById("editProfileButton").style.display = "none";       //Edit-Profile-Knopf hidden
-    document.getElementById("addFriendInputLabel").style.display = "none";          //Freund hinzufügen Label hidden
-    document.getElementById("addFriendInput").style.display = "none";          //Freund hinzufügen Searchbar hidden
-    document.getElementById("addFriendButton").style.display = "none";          //Freund hinzufügen Button hidden
-    //chatStuff hiden
-    document.getElementById("chatWithWhoInputLabel").style.display = "none";
-    document.getElementById("chatWithWhoInput").style.display = "none";
-    document.getElementById("chatWithWhoButton").style.display = "none";
-    document.getElementById("sendMessageInput").style.display = "none";
-    document.getElementById("sendMessageButton").style.display = "none";
-    document.getElementById("chatMessages_div").style.display = "none"; //alles Messages hiden
+document.getElementById("editProfileButton").style.display = "none";       //Edit-Profile-Knopf hidden
+document.getElementById("addFriendInputLabel").style.display = "none";          //Freund hinzufügen Label hidden
+document.getElementById("addFriendInput").style.display = "none";          //Freund hinzufügen Searchbar hidden
+document.getElementById("addFriendButton").style.display = "none";          //Freund hinzufügen Button hidden
+//chatStuff hiden
+document.getElementById("chatWithWhoInputLabel").style.display = "none";
+document.getElementById("chatWithWhoInput").style.display = "none";
+document.getElementById("chatWithWhoButton").style.display = "none";
+document.getElementById("sendMessageInput").style.display = "none";
+document.getElementById("sendMessageButton").style.display = "none";
+document.getElementById("chatMessages_div").style.display = "none"; //alles Messages hiden
 }
 
 function showSensibleStuff() {
-    document.getElementById("editProfileButton").style.display = "block";       //Edit-Profile-Knopf zeigen
-    document.getElementById("addFriendInputLabel").style.display = "inline";           //Freund hinzufügen Label zeigen
-    document.getElementById("addFriendInput").style.display = "inline";          //Freund hinzufügen Searchbar zeigen
-    document.getElementById("addFriendButton").style.display = "inline";         //Freund hinzufügen Button zeigen
-    document.getElementById("backToMyProfileButton").style.display = "none";  //Zurück Button verstecken
-    //chatStuff zeigen
-    document.getElementById("chatWithWhoInputLabel").style.display = "inline";
-    document.getElementById("chatWithWhoInput").style.display = "inline";
-    document.getElementById("chatWithWhoButton").style.display = "inline";
-    if (chatPartner !== undefined) {
-        document.getElementById("sendMessageInput").style.display = "inline";
-        document.getElementById("sendMessageButton").style.display = "inline";
-        document.getElementById("chat_heading").textContent = "Chat mit " + chatPartner.toString().toUpperCase();  //Überschrift mit Username2
-    }
-    document.getElementById("chatMessages_div").style.display = "block"; //alles Messages zeigen
+document.getElementById("editProfileButton").style.display = "block";       //Edit-Profile-Knopf zeigen
+document.getElementById("addFriendInputLabel").style.display = "inline";           //Freund hinzufügen Label zeigen
+document.getElementById("addFriendInput").style.display = "inline";          //Freund hinzufügen Searchbar zeigen
+document.getElementById("addFriendButton").style.display = "inline";         //Freund hinzufügen Button zeigen
+document.getElementById("backToMyProfileButton").style.display = "none";  //Zurück Button verstecken
+//chatStuff zeigen
+document.getElementById("chatWithWhoInputLabel").style.display = "inline";
+document.getElementById("chatWithWhoInput").style.display = "inline";
+document.getElementById("chatWithWhoButton").style.display = "inline";
+if (chatPartner !== undefined) {
+    document.getElementById("sendMessageInput").style.display = "inline";
+    document.getElementById("sendMessageButton").style.display = "inline";
+    document.getElementById("chat_heading").textContent = "Chat mit " + chatPartner.toString().toUpperCase();  //Überschrift mit Username2
+}
+document.getElementById("chatMessages_div").style.display = "block"; //alles Messages zeigen
 }
 
 //löscht Freundesliste um nur Freundesliste des Freundes zu sehen
 function deleteOldFriendList() {
-    const friendList = document.getElementsByClassName("friend-list");
-    for (let i = 0; i < friendList.length; i++) {
-        friendList.item(i).remove();
-    }
+const friendList = document.getElementsByClassName("friend-list");
+for (let i = 0; i < friendList.length; i++) {
+    friendList.item(i).remove();
+}
 }
 
 
 function friendGetUsernameFromDatabase(username) {
-    fetch("/friendGetUsername", {
-        method: 'POST',
-        body: JSON.stringify(username),
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: 'include'
-    }).then(result => result.text())
-        .then(result => document.getElementById("username").textContent = result)
+fetch("/friendGetUsername", {
+    method: 'POST',
+    body: JSON.stringify(username),
+    headers: {
+        "Content-Type": "application/json"
+    },
+    credentials: 'include'
+}).then(result => result.text())
+    .then(result => document.getElementById("username").textContent = result)
 }
 
 function friendGetMailFromDatabase(username) {
-    fetch("/profile/friendGetMail", {
-        method: 'POST',
-        body: JSON.stringify(username),
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: 'include'
-    }).then(result => result.text())
-        .then(result => document.getElementById("mail").textContent = "Email: " + result)
+fetch("/profile/friendGetMail", {
+    method: 'POST',
+    body: JSON.stringify(username),
+    headers: {
+        "Content-Type": "application/json"
+    },
+    credentials: 'include'
+}).then(result => result.text())
+    .then(result => document.getElementById("mail").textContent = "Email: " + result)
 }
 
 function friendGetGesamtpunkteFromDatabase(username) {
-    fetch("/profile/friendGetTotalPoints", {
-        method: 'POST',
-        body: JSON.stringify(username),
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: 'include'
-    }).then(result => result.text())
-        .then(result => document.getElementById("gesamtpunkte").textContent = "Gesamtpunkte: " + result)
+fetch("/profile/friendGetTotalPoints", {
+    method: 'POST',
+    body: JSON.stringify(username),
+    headers: {
+        "Content-Type": "application/json"
+    },
+    credentials: 'include'
+}).then(result => result.text())
+    .then(result => document.getElementById("gesamtpunkte").textContent = "Gesamtpunkte: " + result)
 }
 
 function friendGetHighscoreFromDatabase(username) {
-    fetch("/profile/friendGetHighScore", {
-        method: 'POST',
-        body: JSON.stringify(username),
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: 'include'
-    }).then(result => result.text())
-        .then(result => document.getElementById("highscore").textContent = "Highscore: " + result)
+fetch("/profile/friendGetHighScore", {
+    method: 'POST',
+    body: JSON.stringify(username),
+    headers: {
+        "Content-Type": "application/json"
+    },
+    credentials: 'include'
+}).then(result => result.text())
+    .then(result => document.getElementById("highscore").textContent = "Highscore: " + result)
 }
 
 function friendGetTierFromDatabase(username) {
-    fetch("/profile/friendGetTierName", {
-        method: 'POST',
-        body: JSON.stringify(username),
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: 'include'
-    }).then(result => result.text())
-        .then(result => document.getElementById("tier").textContent = "Rang: " + result)
+fetch("/profile/friendGetTierName", {
+    method: 'POST',
+    body: JSON.stringify(username),
+    headers: {
+        "Content-Type": "application/json"
+    },
+    credentials: 'include'
+}).then(result => result.text())
+    .then(result => document.getElementById("tier").textContent = "Rang: " + result)
 }
 
 function friendGetProfilePicFromDatabase(username) {
-    fetch("/profile/friendGetProfilePic", {
-        method: 'POST',
-        body: JSON.stringify(username),
-        headers: {
-            "Content-Type": "application/json"
-        },
-        credentials: 'include'
-    }).then(result => result.json())
-        .then(result => setFriendsProfilePic(result))
-        .catch((error) => {
-            alert("Couldn't retrieve Profile Picture from database");
-            console.error('Error:', error);
-        });
+fetch("/profile/friendGetProfilePic", {
+    method: 'POST',
+    body: JSON.stringify(username),
+    headers: {
+        "Content-Type": "application/json"
+    },
+    credentials: 'include'
+}).then(result => result.json())
+    .then(result => setFriendsProfilePic(result))
+    .catch((error) => {
+        alert("Couldn't retrieve Profile Picture from database");
+        console.error('Error:', error);
+    });
 }
 
 function setFriendsProfilePic(result) {
-    if (result != null) {
-        document.getElementById("profile-picture").setAttribute("src", result)
-    } else document.getElementById("profile-picture").setAttribute("src", "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png");
+if (result != null) {
+    document.getElementById("profile-picture").setAttribute("src", result)
+} else document.getElementById("profile-picture").setAttribute("src", "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png");
 }
